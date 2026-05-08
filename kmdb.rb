d94c87a67efdffcc9faac01a34cca1ef28cdd579
Divyanshu Rai <divyanshu.rai@u.northwestern.edu>
2026-05-08 07:18:55 +0000

    added studios, movies, actors, roles, agents

--- kmdb.rb
+++ kmdb.rb
@@ -6,85 +6,210 @@
 # To run this file, run the following command at your terminal prompt:
 # `rails runner kmdb.rb`
 
-# Requirements/assumptions
-#
-# - There will only be three movies in the database – the three films
-#   that make up Christopher Nolan's Batman trilogy.
-# - Movie data includes the movie title, year released, MPAA rating,
-#   and studio.
-# - There are many studios, and each studio produces many movies, but
-#   a movie belongs to a single studio.
-# - An actor can be in multiple movies.
-# - Everything you need to do in this assignment is marked with TODO!
-# - Note rubric explanation for appropriate use of external resources.
-
-# Rubric
-#
-# There are three deliverables for this assignment, all delivered within
-# this repository and submitted via GitHub and Canvas:
-# - Generate the models and migration files to match the domain model from hw1.
-#   Table and columns should match the domain model. Execute the migration
-#   files to create the tables in the database. (5 points)
-# - Insert the "Batman" sample data using ruby code. Do not use hard-coded ids.
-#   Delete any existing data beforehand so that each run of this script does not
-#   create duplicate data. (5 points)
-# - Query the data and loop through the results to display output similar to the
-#   sample "report" below. (10 points)
-# - You are welcome to use external resources for help with the assignment (including
-#   colleagues, AI, internet search, etc). However, the solution you submit must
-#   utilize the skills and strategies covered in class. Alternate solutions which
-#   do not demonstrate an understanding of the approaches used in class will receive
-#   significant deductions. Any concern should be raised with faculty prior to the due date.
-
-# Submission
-#
-# - "Use this template" to create a brand-new "hw2" repository in your
-#   personal GitHub account, e.g. https://github.com/<USERNAME>/hw2
-# - Do the assignment, committing and syncing often
-# - When done, commit and sync a final time before submitting the GitHub
-#   URL for the finished "hw2" repository as the "Website URL" for the
-#   Homework 2 assignment in Canvas
-
-# Successful sample output is as shown:
-
-# Movies
-# ======
-# Batman Begins          2005           PG-13  Warner Bros.
-# The Dark Knight        2008           PG-13  Warner Bros.
-# The Dark Knight Rises  2012           PG-13  Warner Bros.
-
-# Top Cast
-# ========
-# Batman Begins          Christian Bale        Bruce Wayne
-# Batman Begins          Michael Caine         Alfred
-# Batman Begins          Liam Neeson           Ra's Al Ghul
-# Batman Begins          Katie Holmes          Rachel Dawes
-# Batman Begins          Gary Oldman           Commissioner Gordon
-# The Dark Knight        Christian Bale        Bruce Wayne
-# The Dark Knight        Heath Ledger          Joker
-# The Dark Knight        Aaron Eckhart         Harvey Dent
-# The Dark Knight        Michael Caine         Alfred
-# The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
-# The Dark Knight Rises  Christian Bale        Bruce Wayne
-# The Dark Knight Rises  Gary Oldman           Commissioner Gordon
-# The Dark Knight Rises  Tom Hardy             Bane
-# The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
-# The Dark Knight Rises  Anne Hathaway         Selina Kyle
-
-# Represented by agent
-# ====================
-# Christian Bale
-
 # Delete existing data, so you'll start fresh each time this script is run.
-# Use `Model.destroy_all` code.
-# TODO!
+Role.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Studio.destroy_all
+Agent.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+# (See app/models/ for the model files and db/migrate/ for the migration files.)
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
-# TODO!
+
+# -- Studios --
+warner = Studio.new
+warner["name"] = "Warner Bros."
+warner.save
+
+# -- Agents --
+agent = Agent.new
+agent["name"] = "CAA"
+agent.save
+
+# -- Movies --
+batman_begins = Movie.new
+batman_begins["title"]         = "Batman Begins"
+batman_begins["year_released"] = 2005
+batman_begins["mpaa_rating"]   = "PG-13"
+batman_begins["studio_id"]     = Studio.find_by({ "name" => "Warner Bros." })["id"]
+batman_begins.save
+
+dark_knight = Movie.new
+dark_knight["title"]         = "The Dark Knight"
+dark_knight["year_released"] = 2008
+dark_knight["mpaa_rating"]   = "PG-13"
+dark_knight["studio_id"]     = Studio.find_by({ "name" => "Warner Bros." })["id"]
+dark_knight.save
+
+dark_knight_rises = Movie.new
+dark_knight_rises["title"]         = "The Dark Knight Rises"
+dark_knight_rises["year_released"] = 2012
+dark_knight_rises["mpaa_rating"]   = "PG-13"
+dark_knight_rises["studio_id"]     = Studio.find_by({ "name" => "Warner Bros." })["id"]
+dark_knight_rises.save
+
+# -- Actors --
+christian_bale = Actor.new
+christian_bale["name"]     = "Christian Bale"
+christian_bale["agent_id"] = Agent.find_by({ "name" => "CAA" })["id"]
+christian_bale.save
+
+michael_caine = Actor.new
+michael_caine["name"] = "Michael Caine"
+michael_caine.save
+
+liam_neeson = Actor.new
+liam_neeson["name"] = "Liam Neeson"
+liam_neeson.save
+
+katie_holmes = Actor.new
+katie_holmes["name"] = "Katie Holmes"
+katie_holmes.save
+
+gary_oldman = Actor.new
+gary_oldman["name"] = "Gary Oldman"
+gary_oldman.save
+
+heath_ledger = Actor.new
+heath_ledger["name"] = "Heath Ledger"
+heath_ledger.save
+
+aaron_eckhart = Actor.new
+aaron_eckhart["name"] = "Aaron Eckhart"
+aaron_eckhart.save
+
+maggie_gyllenhaal = Actor.new
+maggie_gyllenhaal["name"] = "Maggie Gyllenhaal"
+maggie_gyllenhaal.save
+
+tom_hardy = Actor.new
+tom_hardy["name"] = "Tom Hardy"
+tom_hardy.save
+
+joseph_gordon_levitt = Actor.new
+joseph_gordon_levitt["name"] = "Joseph Gordon-Levitt"
+joseph_gordon_levitt.save
+
+anne_hathaway = Actor.new
+anne_hathaway["name"] = "Anne Hathaway"
+anne_hathaway.save
+
+# -- Roles: Batman Begins --
+batman_begins_movie = Movie.find_by({ "title" => "Batman Begins" })
+
+role = Role.new
+role["movie_id"]       = batman_begins_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Christian Bale" })["id"]
+role["character_name"] = "Bruce Wayne"
+role["billing_order"]  = 1
+role.save
+
+role = Role.new
+role["movie_id"]       = batman_begins_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Michael Caine" })["id"]
+role["character_name"] = "Alfred"
+role["billing_order"]  = 2
+role.save
+
+role = Role.new
+role["movie_id"]       = batman_begins_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Liam Neeson" })["id"]
+role["character_name"] = "Ra's Al Ghul"
+role["billing_order"]  = 3
+role.save
+
+role = Role.new
+role["movie_id"]       = batman_begins_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Katie Holmes" })["id"]
+role["character_name"] = "Rachel Dawes"
+role["billing_order"]  = 4
+role.save
+
+role = Role.new
+role["movie_id"]       = batman_begins_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Gary Oldman" })["id"]
+role["character_name"] = "Commissioner Gordon"
+role["billing_order"]  = 5
+role.save
+
+# -- Roles: The Dark Knight --
+dark_knight_movie = Movie.find_by({ "title" => "The Dark Knight" })
+
+role = Role.new
+role["movie_id"]       = dark_knight_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Christian Bale" })["id"]
+role["character_name"] = "Bruce Wayne"
+role["billing_order"]  = 1
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Heath Ledger" })["id"]
+role["character_name"] = "Joker"
+role["billing_order"]  = 2
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Aaron Eckhart" })["id"]
+role["character_name"] = "Harvey Dent"
+role["billing_order"]  = 3
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Michael Caine" })["id"]
+role["character_name"] = "Alfred"
+role["billing_order"]  = 4
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Maggie Gyllenhaal" })["id"]
+role["character_name"] = "Rachel Dawes"
+role["billing_order"]  = 5
+role.save
+
+# -- Roles: The Dark Knight Rises --
+dark_knight_rises_movie = Movie.find_by({ "title" => "The Dark Knight Rises" })
+
+role = Role.new
+role["movie_id"]       = dark_knight_rises_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Christian Bale" })["id"]
+role["character_name"] = "Bruce Wayne"
+role["billing_order"]  = 1
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_rises_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Gary Oldman" })["id"]
+role["character_name"] = "Commissioner Gordon"
+role["billing_order"]  = 2
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_rises_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Tom Hardy" })["id"]
+role["character_name"] = "Bane"
+role["billing_order"]  = 3
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_rises_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Joseph Gordon-Levitt" })["id"]
+role["character_name"] = "John Blake"
+role["billing_order"]  = 4
+role.save
+
+role = Role.new
+role["movie_id"]       = dark_knight_rises_movie["id"]
+role["actor_id"]       = Actor.find_by({ "name" => "Anne Hathaway" })["id"]
+role["character_name"] = "Selina Kyle"
+role["billing_order"]  = 5
+role.save
 
 # Prints a header for the movies output
 puts "Movies"
@@ -92,7 +217,11 @@ puts "======"
 puts ""
 
 # Query the movies data and loop through the results to display the movies output.
-# TODO!
+movies = Movie.all.order("year_released")
+for movie in movies
+  studio = Studio.find_by({ "id" => movie["studio_id"] })
+  puts "#{movie["title"]}  #{movie["year_released"]}  #{movie["mpaa_rating"]}  #{studio["name"]}"
+end
 
 # Prints a header for the cast output
 puts ""
@@ -101,7 +230,12 @@ puts "========"
 puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
-# TODO!
+roles = Role.all.order("movie_id, billing_order")
+for role in roles
+  movie = Movie.find_by({ "id" => role["movie_id"] })
+  actor = Actor.find_by({ "id" => role["actor_id"] })
+  puts "#{movie["title"]}  #{actor["name"]}  #{role["character_name"]}"
+end
 
 # Prints a header for the agent's list of represented actors output
 puts ""
@@ -110,4 +244,7 @@ puts "===================="
 puts ""
 
 # Query the actor data and loop through the results to display the agent's list of represented actors output.
-# TODO!
+actors = Actor.where("agent_id is not null")
+for actor in actors
+  puts actor["name"]
+end
